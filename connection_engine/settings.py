b8efd9001cfcd2d1d2fe943933faf4be230fb09d
SoroushGReza <soroush_developer@hotmail.com>
2025-03-23 18:05:34 +0300

    Customized email sending for password reset:
    settings.py:
    - Added django.contrib.sites to INSTALLED_APPS   Added
      `"django.contrib.sites"` to the `INSTALLED_APPS` list.
    
    templates\account\email\password_reset_key_message.html: (NEW FILE)
    - Styled HTML template for password reset email   Created an HTML
      version of the password reset email with branding and improved
      structure. Included a logo, more personalized text, and a cleaner
      format.
    
    templates\account\email\password_reset_key_message.txt:
    - Restructured and simplified the plain text version of the password
      reset email   Simplified and personalized the plain text version of
      the email. Removed block tags and made the message clearer, added a
      greeting, and improved the flow.
    
    user_profile\adapter.py:
    - Overridden send_mail method to support HTML emails for password reset
      Extended `send_mail` in `CustomAccountAdapter` to handle both plain
      text and HTML formats for password reset.
      - Rendered subject and message bodies from templates.
      - Used `EmailMultiAlternatives` to send both plain text and HTML
        versions of the email.
      - Set a default sender email if not defined in settings.

--- connection_engine/settings.py
+++ connection_engine/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "unfold.contrib.import_export",
     "django.contrib.admin",
     "django.contrib.auth",
+    "django.contrib.sites",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
