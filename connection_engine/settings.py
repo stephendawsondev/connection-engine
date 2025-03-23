0c027a45a8d7c695f1afd331cc7f0ff4dac7c8f4
SoroushGReza <soroush_developer@hotmail.com>
2025-03-23 16:38:11 +0300

    Add email config using environment variables
    Delete "retro-star.svg" file (not in use)

--- connection_engine/settings.py
+++ connection_engine/settings.py
@@ -98,6 +98,15 @@ ACCOUNT_FORMS = {
     "signup": "user_profile.forms.CustomSignupForm",
 }
 
+# Email configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", "")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "") == "True"
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
 
 TEMPLATES = [
     {
