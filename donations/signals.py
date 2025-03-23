4827f4af944eab10823ef844d62c4543a3c2f6fa
Stephen Dawson <stephendawson@protonmail.com>
2025-03-23 08:49:05 +0000

    fix: remove signal to fix recursion error

--- donations/signals.py
+++ donations/signals.py
@@ -14,12 +14,3 @@ def generate_confirmation_number(sender, instance, created, **kwargs):
     if created and not instance.confirmation_number:
         instance.confirmation_number = uuid.uuid4().hex.upper()
         instance.save()
-
-
-@receiver(post_save, sender=Payment)
-def update_grand_total(sender, instance, **kwargs):
-    """
-    Update the grand total based on the amount.
-    """
-    instance.grand_total = instance.amount
-    instance.save()
