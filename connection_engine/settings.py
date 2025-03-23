04f7c070b22bfa9647b09c6ff2e03c52b5e15b0d
SoroushGReza <soroush_developer@hotmail.com>
2025-03-23 16:52:24 +0300

    Moved non-sensitive settings to settings.py:
    Moved: EMAIL_HOST, EMAIL_PORT, and EMAIL_USE_TLS from env.py into
    settings.py to keep only the sensitive info in environment variables.

--- connection_engine/settings.py
+++ connection_engine/settings.py
@@ -99,12 +99,12 @@ ACCOUNT_FORMS = {
 }
 
 # Email configuration
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", "")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "") == "True"
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 
