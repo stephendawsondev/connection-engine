c04292e2bf94be336613662290d789623199f51d
Warren Smith <warren.lee.smith@hotmail.co.uk>
2025-03-24 10:27:13 +0000

    UPDATE: Sponsorship page

--- donations/views.py
+++ donations/views.py
@@ -258,6 +258,7 @@ class WomenInTechListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["profile_user"] = self.request.user
+        context["stripe_publishable_key"] = settings.STRIPE_PUBLISHABLE_KEY
         return context
 
 
